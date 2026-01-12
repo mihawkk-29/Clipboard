2e0d23673108eef049393927264d2e5692527f50
Aarun <you@example.com>
2026-01-12 23:39:24 +0530

    completed stage one

--- src/clipBoard.java
+++ src/clipBoard.java
@@ -2,16 +2,27 @@ import java.awt.*;
 import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.io.IOException;
 
 public class clipBoard {
 
     clipBoard() {
+
         Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
-        System.out.println("Clipboard Connected");
+        //System.out.println("Clipboard Connected");
+        Transferable data = clipboard.getContents(null);
 
-        if(clipboard.isDataFlavorAvailable(DataFlavor.stringFlavor)){
+        if (data != null && data.isDataFlavorSupported(DataFlavor.stringFlavor)) {
+            try {
 
-            String text = (String)
+                String text = (String) data.getTransferData(DataFlavor.stringFlavor);
+                System.out.println(text);
+            }
+            catch (UnsupportedFlavorException | IOException e ) {
+                e.printStackTrace();
+            }
         }
     }
 }
+
